895ab3a69174222a0653c5c90296040d97cbfae8
Hob Adams <hello@hobadams.com>
2020-02-25 10:53:53 +0000

    fix homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,53 +3,7 @@ import classnames from 'classnames';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
-
-const features = [
-  {
-    title: <>Falcon Platform</>,
-    imageUrl: 'img/deity-logo.svg',
-    description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </>
-    ),
-  },
-  {
-    title: <>DEITY Cloud</>,
-    imageUrl: 'img/deity-logo.svg',
-    description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </>
-    ),
-  },
-  {
-    title: <>Falcon Client</>,
-    imageUrl: 'img/deity-logo.svg',
-    description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </>
-    ),
-  },
-];
-
-function Feature({ imageUrl, title, description }) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={classnames('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
 
 function Home() {
   const context = useDocusaurusContext();
@@ -59,24 +13,42 @@ function Home() {
       title={`${siteConfig.title}`}
       description="DEITY documentation and user guides"
     >
-      <header className={classnames("hero hero--theme", styles.heroBanner)}>
+      <header className={classnames("hero hero--theme", styles.heroBanner)} style={{ backgroundImage: "url('img/pattern-honeycomb-secondary.svg')" }}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
         </div>
       </header>
       <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
+        <section className={styles.about}>
+          <div className={classnames("container", styles.aboutContainer)}>
+            <div className={styles.aboutColumn}>
+              <h3>What is Falcon Platform?</h3>
+              <p>The Falcon Platform is a fully packed hosted front-end platform, which allows to create the best front-end experiences for eCommerce.</p>
+              <p>Falcon is not just a PWA theme, it is a complete platform to build the best shopping experience with top performance and unlimited flexibility, enabling your future growth.</p>
+              <Link
+                href="/"
+                target="_blank"
+                className="button"
+                rel="noreferrer noopener"
+                aria-label="Find out more about the Falcon Platform"
+              >Find out more</Link>
             </div>
-          </section>
-        )}
+            <div className={styles.aboutColumn}>
+              <img
+                srcset="img/hero-pwa-falcon-700.png 700w,
+                img/hero-pwa-falcon-1200.png 1200w"
+                sizes="(max-width: 600px) 700px, 120px"
+                loading="lazy"
+                width="1200px"
+                height="1197px"
+                src="img/hero-pwa-falcon-700.png"
+                alt="Deity Falcon illustration"
+                className={styles.aboutImg}
+              />
+            </div>
+          </div>
+        </section>
         <section className={styles.infoBanner}>
           <div className="container">
             <div className={styles.infoBanner__content}>
