f2c2a6ed901a53c5e0c49eb218ccdd80af8cb514
Borales <bordun.alexandr@gmail.com>
2021-03-05 14:09:47 +0100

    Adding "debug" flag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,199 +1,199 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  title: "DEITY Falcon",
-  tagline: "Documentation",
-  url: "https://deity.io",
-  baseUrl: "/",
-  favicon: "img/favicon.png",
-  organizationName: "DEITY", // Usually your GitHub org/user name.
-  projectName: "falcon-platform-docs", // Usually your repo name.
+  title: 'DEITY Falcon',
+  tagline: 'Documentation',
+  url: 'https://deity.io',
+  baseUrl: '/',
+  favicon: 'img/favicon.png',
+  organizationName: 'DEITY', // Usually your GitHub org/user name.
+  projectName: 'falcon-platform-docs', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: "Falcon Documentation",
+      title: 'Falcon Documentation',
       logo: {
-        alt: "DEITY Logo",
-        src: "img/deity-logo.svg",
+        alt: 'DEITY Logo',
+        src: 'img/deity-logo.svg'
       },
       items: [
         {
-          to: "/docs/platform/overview/about",
-          label: "Falcon Platform",
-          position: "right",
-          activeBasePath: "/docs/platform/",
+          to: '/docs/platform/overview/about',
+          label: 'Falcon Platform',
+          position: 'right',
+          activeBasePath: '/docs/platform/'
         },
         {
-          label: "Falcon UI",
-          href: "https://falcon-ui.docs.deity.io/",
-          position: "right",
-        },
-      ],
+          label: 'Falcon UI',
+          href: 'https://falcon-ui.docs.deity.io/',
+          position: 'right'
+        }
+      ]
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Our documentation",
+          title: 'Our documentation',
           items: [
             {
-              label: "Falcon Platform",
-              to: "/docs/platform/overview/about",
+              label: 'Falcon Platform',
+              to: '/docs/platform/overview/about'
             },
             {
-              label: "Falcon Cloud",
-              to: "/docs/platform/cloud/about",
+              label: 'Falcon Cloud',
+              to: '/docs/platform/cloud/about'
             },
             {
-              label: "Falcon 2019",
-              to: "/docs/2019/getting-started/intro",
+              label: 'Falcon 2019',
+              to: '/docs/2019/getting-started/intro'
             },
             {
-              label: "Falcon UI",
-              href: "https://falcon-ui.docs.deity.io/",
-            },
-          ],
+              label: 'Falcon UI',
+              href: 'https://falcon-ui.docs.deity.io/'
+            }
+          ]
         },
         {
-          title: "Our Integrations",
+          title: 'Our Integrations',
           items: [
             {
-              label: "Algolia",
-              to: "/docs/platform/integration/algolia",
+              label: 'Algolia',
+              to: '/docs/platform/integration/algolia'
             },
             {
-              label: "BigCommerce",
-              to: "/docs/platform/integration/bigcommerce/overview",
+              label: 'BigCommerce',
+              to: '/docs/platform/integration/bigcommerce/overview'
             },
             {
-              label: "Magento 2",
-              to: "/docs/platform/integration/magento2/overview",
+              label: 'Magento 2',
+              to: '/docs/platform/integration/magento2/overview'
             },
             {
-              label: "Stripe",
-              to: "/docs/platform/integration/stripe",
+              label: 'Stripe',
+              to: '/docs/platform/integration/stripe'
             },
             {
-              label: "Mollie",
-              to: "/docs/platform/integration/mollie",
+              label: 'Mollie',
+              to: '/docs/platform/integration/mollie'
             },
             {
-              label: "Wordpress",
-              to: "/docs/platform/integration/wordpress",
-            },
-          ],
+              label: 'Wordpress',
+              to: '/docs/platform/integration/wordpress'
+            }
+          ]
         },
         {
-          title: "Community & Help",
+          title: 'Community & Help',
           items: [
             {
-              label: "Community Chat",
-              href: "http://slack.deity.io/",
+              label: 'Community Chat',
+              href: 'http://slack.deity.io/'
             },
             {
-              label: "Twitter",
-              href: "https://twitter.com/deity_pwa",
+              label: 'Twitter',
+              href: 'https://twitter.com/deity_pwa'
             },
             {
-              label: "YouTube",
-              href: "https://www.youtube.com/channel/UCCyszDV63yrqFHUY1uWf4mQ",
+              label: 'YouTube',
+              href: 'https://www.youtube.com/channel/UCCyszDV63yrqFHUY1uWf4mQ'
             },
             {
-              label: "Contact Us",
-              href: "https://deity.io/contact",
-            },
-          ],
+              label: 'Contact Us',
+              href: 'https://deity.io/contact'
+            }
+          ]
         },
         {
-          title: "Useful Links",
+          title: 'Useful Links',
           items: [
             {
-              label: "Marketing Site",
-              href: "https://deity.io/",
+              label: 'Marketing Site',
+              href: 'https://deity.io/'
             },
             {
-              label: "Status",
-              href: "https://status.deity.io/",
+              label: 'Status',
+              href: 'https://status.deity.io/'
             },
             {
-              label: "Blog",
-              href: "https://medium.com/deity-io",
+              label: 'Blog',
+              href: 'https://medium.com/deity-io'
             },
             {
-              label: "Privacy Policy",
-              href: "https://deity.io/privacy-policy",
-            },
-          ],
-        },
+              label: 'Privacy Policy',
+              href: 'https://deity.io/privacy-policy'
+            }
+          ]
+        }
       ],
       logo: {
-        alt: "DEITY Logo",
-        src: "img/deity-logo.svg",
+        alt: 'DEITY Logo',
+        src: 'img/deity-logo.svg'
       },
-      copyright: `Copyright © ${new Date().getFullYear()} DEITY B.V.`,
+      copyright: `Copyright © ${new Date().getFullYear()} DEITY B.V.`
     },
-    image: "img/deity-logo.svg",
+    image: 'img/deity-logo.svg',
 
     algolia: {
-      apiKey: "3cd0500212418e98ed4f90579abbe0fb",
-      indexName: "deity",
-      algoliaOptions: {},
+      apiKey: '3cd0500212418e98ed4f90579abbe0fb',
+      indexName: 'deity',
+      algoliaOptions: {}
     },
     mailchimp: {
-      submitUrl: "https://Deity.us16.list-manage.com/subscribe/post-json?u=6c94229e24ca6964641a9d054&id=f7e67a2b7d"
+      submitUrl: 'https://Deity.us16.list-manage.com/subscribe/post-json?u=6c94229e24ca6964641a9d054&id=f7e67a2b7d'
     },
     intercom: {
-      appId: "z91ewqiv",
+      appId: 'z91ewqiv'
     }
   },
   plugins: [
     [
-      path.resolve(__dirname, "./src/plugins/deity-gtm/src/index.js"),
+      path.resolve(__dirname, './src/plugins/deity-gtm/src/index.js'),
       {
-        containerId: "GTM-N4NQJGX",
-      },
+        containerId: 'GTM-N4NQJGX'
+      }
     ],
     [
-      "@docusaurus/plugin-pwa",
+      '@docusaurus/plugin-pwa',
       {
-        offlineModeActivationStrategies: ["always"],
+        offlineModeActivationStrategies: ['always'],
+        debug: false,
         pwaHead: [
           {
-            tagName: "link",
-            rel: "icon",
-            href: "/img/logo-512.png",
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/logo-512.png'
           },
           {
-            tagName: "link",
-            rel: "manifest",
-            href: "/manifest.json",
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json'
           },
           {
-            tagName: "meta",
-            name: "theme-color",
-            content: "#26642c",
+            tagName: 'meta',
+            name: 'theme-color',
+            content: '#26642c'
           },
           {
-            tagName: "link",
-            rel: "apple-touch-icon",
-            href: "/img/logo-192.png",
-          },
-        ],
-      },
-    ],
+            tagName: 'link',
+            rel: 'apple-touch-icon',
+            href: '/img/logo-192.png'
+          }
+        ]
+      }
+    ]
   ],
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl:
-            "https://github.com/deity-io/falcon-platform-docs/edit/master",
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/deity-io/falcon-platform-docs/edit/master'
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
-    ],
-  ],
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      }
+    ]
+  ]
 };
