ca2e12aabec141c0cecbf5d4ed23c9b98058124e
Borales <bordun.alexandr@gmail.com>
2021-03-05 15:57:39 +0100

    Disabling gtm

--- src/plugins/deity-gtm/src/index.js
+++ src/plugins/deity-gtm/src/index.js
@@ -4,17 +4,17 @@ module.exports = function (context, options) {
     injectHtmlTags() {
       return {
         headTags: [
-          {
+          /*{
             tagName: "script",
             innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','${options.containerId}');`
-          }
+          } */
         ],
         postBodyTags: [
-          {
+          /* {
             tagName: "noscript",
             innerHTML: `
             <iframe
@@ -23,9 +23,9 @@ module.exports = function (context, options) {
               width="0"
               style="display: none, visibility: hidden"
             ></iframe>`
-          }
-        ],
+          } */
+        ]
       };
-    },
+    }
   };
 };
