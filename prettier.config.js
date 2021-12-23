3c7134121c5ed207d4f777a543aea08bc341aa36
Mateusz Woźniak <kontakt@mateuszwozniak.pl>
2021-12-23 16:28:17 +0100

    Remove @deity/eslint-config-falcon as Netlify cannot use NPM_TOKEN from env variable - copy the config from @deity/eslint-config-falcon@2.7.23

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
-const esLint = require('@deity/eslint-config-falcon');
+const esLint = require('./.eslint-config-falcon-2-7-23.json');
 
 module.exports = Object.assign({}, esLint.rules['prettier/prettier'][1], {
   overrides: [
