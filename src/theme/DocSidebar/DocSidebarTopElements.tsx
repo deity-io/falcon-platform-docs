ddc1c4a6ac5498ef462c4f019ed9db16de7e1861
Krystian Czyz <krystian@deity.io>
2021-12-30 15:22:48 +0100

    fix sidebar styles in preview

--- src/theme/DocSidebar/DocSidebarTopElements.tsx
+++ src/theme/DocSidebar/DocSidebarTopElements.tsx
@@ -13,25 +13,13 @@ const DocSidebarTopElements: FC<Props> = ({ enabled }) => (
     {enabled ? (
       <>
         <li>
-          <a
-            href="/"
-            className={classNames(
-              'menuLink_src-theme-DocSidebarItem-styles-module menuLinkSublist_src-theme-DocSidebarItem-styles-module',
-              styles.backLink
-            )}
-          >
+          <a href="/" className={classNames(styles.menuLink, styles.backLink)}>
             {iconMap['All docs']}
             All docs
           </a>
         </li>
         <li>
-          <a
-            href="#"
-            className={classNames(
-              'menuLink_src-theme-DocSidebarItem-styles-module menuLinkSublist_src-theme-DocSidebarItem-styles-module',
-              styles.playground
-            )}
-          >
+          <a href="#" className={classNames(styles.menuLink, styles.playground)}>
             GraphQL Playground
             {iconMap['GraphQL Playground']}
           </a>
