afce3e0b5689f5bd8beb03f8e343dd586b9d3a6a
Krystian Czyz <krystian@deity.io>
2023-02-20 12:01:35 +0100

    DOCS-5 add description to the changelog

--- src/components/Tabs/ChangelogItem.tsx
+++ src/components/Tabs/ChangelogItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss';
 import Check from '@site/static/img/icons/check.svg';
 import clsx from 'clsx';
@@ -6,22 +6,66 @@ import clsx from 'clsx';
 type Props = {
   variant: 'breaking' | 'hotfix' | 'feature' | 'improvement' | 'bugfix' | 'deprecated';
   children: React.ReactNode;
+  description?: JSX.Element;
 };
 
-function ChangelogItem({ children, variant }: Props): JSX.Element {
+function ChangelogItem({ children, variant, description }: Props): JSX.Element {
+  const [isOpen, setIsOpen] = useState(false);
+  const divRef = useRef(null);
+  const [isOverflowed, setIsOverflowed] = useState(false);
+
+  useEffect(() => {
+    function handleOverflow() {
+      if (divRef.current) {
+        const { scrollHeight, clientHeight } = divRef.current;
+        setIsOverflowed(scrollHeight > clientHeight);
+      }
+    }
+    function handleResize() {
+      handleOverflow();
+    }
+
+    handleOverflow();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  console.log(isOverflowed);
+
   return (
     <div className={styles.item}>
-      <div className={styles.content}>
-        <Check className={styles.icon} />
-        {children}
+      <div className={styles.header}>
+        <div className={styles.content}>
+          <Check className={styles.icon} />
+          {children}
+        </div>
+        <div
+          className={clsx(styles.badge, {
+            [styles[variant]]: variant
+          })}
+        >
+          {variant}
+        </div>
       </div>
-      <div
-        className={clsx(styles.badge, {
-          [styles[variant]]: variant
-        })}
-      >
-        {variant}
-      </div>
+      {description && (
+        <div className={styles.description}>
+          <div
+            ref={divRef}
+            className={clsx({
+              [styles.hiddenDescription]: !isOpen
+            })}
+          >
+            {description}
+          </div>
+          {isOverflowed && (
+            <div className={styles.toggleButton} onClick={() => setIsOpen(prev => !prev)}>
+              {isOpen ? 'Hide details' : 'Show details'}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
